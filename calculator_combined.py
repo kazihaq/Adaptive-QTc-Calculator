123dc78354fa708a219266cdf3c65695b79d3f7d
kazihaq <46686679+kazihaq@users.noreply.github.com>
2025-06-30 16:58:19 -0400

    Update calculator_combined.py

--- calculator_combined.py
+++ calculator_combined.py
@@ -21,20 +21,16 @@ import base64
 
 def set_background(image_file):
     with open(image_file, "rb") as file:
-        encoded_string = base64.b64encode(file.read()).decode()
-    st.markdown(
-        f"""
-        <style>
-        .stApp {{
-            background-image: url("data:image/png;base64,{encoded_string}");
-            background-size: cover;
-            background-repeat: no-repeat;
-            background-attachment: fixed;
-        }}
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
+        encoded = base64.b64encode(file.read()).decode()
+    css = f"""
+    <style>
+    .stApp {{
+        background-image: url("data:image/png;base64,{encoded}");
+        background-size: cover;
+    }}
+    </style>
+    """
+    st.markdown(css, unsafe_allow_html=True)
 
 # Set the background (put this near the top of your script)
 set_background("background.png")
